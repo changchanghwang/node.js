430bdd1212766df9c677c7b01f9e1623b4a3d1b7
CH_Hwang <hch950627@naver.com>
2021-08-04 01:58:35 +0900

    nodejs 생활코딩 1일차

---
+++ syntax/string.js
@@ -0,0 +1,3 @@
+console.log(1+1);
+console.log('1'+'1');
+console.log('asfdasdfasfdsdfafsdsfd sfdfasdasdfasdffd fdsaasfdssfdasdf afdsdsasdfadsfafsd fdsaasfddsaffsdasdfafdssfd saasdfafdsafdsafsd fdasfdassdffsddfs asdfdafsfsddsffds fdsafdsfdsfdsaa'.length)
---
+++ syntax/template.js
@@ -0,0 +1,15 @@
+let name = 'hwang'
+let letter = 'dear '+name+' \n\n\
+asdfasdfasdsdfsdafsfsdfasdfsdf\n\
+sdfaasdfsdfsdfsfasdfasdfasdfsadfasdfasdf\n\
+dsafasdfasf'+name;
+
+console.log(letter)
+
+letter = `dear ${name}
+
+asdfasdfasdsdfsdafsfsdfasdfsdf
+sdfaasdfsdfsdfsfasdfasdfasdfsadfasdfasdf
+dsafasdfasf${name}`;
+
+console.log(letter)
