79ec9230321675ccf4d352c274a015b2db0dc900
CH_Hwang <hch950627@naver.com>
2021-08-05 14:55:01 +0900

    nodejs 생활코딩3일차

---
+++ syntax/function.js
@@ -0,0 +1,19 @@
+// function f123(){
+//     console.log(1);
+//     console.log(2);
+//     console.log(3);
+// }
+
+// f123();
+
+console.log(Math.round (1.5)); //2 반올림
+console.log(Math.round (1.4)); //1 반올림
+
+function sum(a,b){ //parameter (매개변수)
+    console.log('a')
+    return a+b; //a+b를 출력하면서 함수 종료
+    console.log('b')
+}
+
+console.log(sum(2,4)); //2랑 4는 argument(인자)
+
