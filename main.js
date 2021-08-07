30af9585dd902d8bd5e5b81c42bdd7b151defadd
CH_Hwang <hch950627@naver.com>
2021-08-07 21:35:31 +0900

    nodejs 생활코딩 5일차

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body){
   return `
@@ -15,6 +16,7 @@ function templateHTML(title, list, body){
           <body>
           </body>
           ${list}
+          <a href="/create">create</a>
           ${body}
         </body>
       </html>
@@ -28,7 +30,6 @@ function templateList(filelist){
   list = list+'</ul>';
   return list;
 }
-
 var app = http.createServer(function(request,response){
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
@@ -49,7 +50,46 @@ var app = http.createServer(function(request,response){
      })
     })
       
-  }else{
+  }else if(pathname ==='/create'){
+    fs.readdir('./data', function(err, filelist){
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        if(title === undefined){
+          title = 'WEB - create';
+        }
+        var list = templateList(filelist)
+        var template = templateHTML(title, list, `
+          <form action="http://localhost:3000/create_process" method="post">
+          <p><input type="text" name="title" placeholder="title"></p>
+          <p>
+              <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+              <input type="submit">
+          </p>
+          </form>
+        `);
+        response.writeHead(200);
+        response.end(template);
+     })
+    })
+  } else if(pathname ==='/create_process'){
+    var body = '';
+    request.on('data', function(data){
+      body += data;
+      if (body.length >1e6){
+        request.connection.destroy();
+      }
+    });
+    request.on('end', function(){
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description
+      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+        response.writeHead(302, {Location:`/?id=${title}`});
+        response.end('Success');
+      });
+    });
+  } else{
     response.writeHead(404);
     response.end('Not found');
   }
